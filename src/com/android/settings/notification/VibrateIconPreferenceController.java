eb59a02811791539287ef540c1ef82cf133b01fa
johnmart19 <johnivan19999@gmail.com>
2022-06-26 20:36:01 +0300

    Settings: Set "Show the vibrate icon in the Status Bar" by default ON
    Change-Id: Ie3db99602e21afdd342f8e81772abd8e0cbbd458

--- src/com/android/settings/notification/VibrateIconPreferenceController.java
+++ src/com/android/settings/notification/VibrateIconPreferenceController.java
@@ -32,7 +32,7 @@ public class VibrateIconPreferenceController extends SettingPrefController {
             Lifecycle lifecycle) {
         super(context, parent, lifecycle);
         mPreference = new SettingPref(
-            TYPE_SECURE, KEY_VIBRATE_ICON, Secure.STATUS_BAR_SHOW_VIBRATE_ICON, 0 /*default off*/);
+            TYPE_SECURE, KEY_VIBRATE_ICON, Secure.STATUS_BAR_SHOW_VIBRATE_ICON, 1 /*default on*/);
     }
 
     @Override
